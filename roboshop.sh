70c9aac1d9ba85a74565c755f4e83d138c9eb63d
karna <karunakark1729@gmail.com>
2025-05-25 17:37:03 +0530

    Shell-Roboshop-Instance IP

--- roboshop.sh
+++ roboshop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-"
+SG_ID="sg-0738b5ce8c030b4a6"
 INSTANCES=("mongodb" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="ZO"
+ZONE_ID="Z06554703QA84OQ4WNDV1"
 DOMAIN_NAME="karna.fun"
 
 for instance in ${INSTANCES[@]}; do
-    INSTANCE-ID=$(aws ec2 run-instances --image-id ami-09 --instance-type t2.micro --security-group-ids sg-01 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09 --instance-type t2.micro --security-group-ids sg-01 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "InstanceId" --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
