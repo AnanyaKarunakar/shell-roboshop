e4497bd43a5b653f70bd8002accd992a97731117
karna <karunakark1729@gmail.com>
2025-05-25 17:41:32 +0530

    Shell-Roboshop-Instance IP

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ ZONE_ID="Z06554703QA84OQ4WNDV1"
 DOMAIN_NAME="karna.fun"
 
 for instance in ${INSTANCES[@]}; do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09 --instance-type t2.micro --security-group-ids sg-01 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0738b5ce8c030b4a6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "InstanceId" --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
