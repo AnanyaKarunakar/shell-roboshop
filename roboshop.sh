d7bbccf845262a9432321091a25e7e3e63c4219e
unknown <karunakark1729@gmail.com>
2025-05-27 13:29:59 +0530

    Creating or Updating a record

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z06554703QA84OQ4WNDV1"
 DOMAIN_NAME="karna.fun"
 
-for instance in ${INSTANCES[@]}; do
+# for instance in ${INSTANCES[@]}
+for instance in $@; do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0738b5ce8c030b4a6 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
