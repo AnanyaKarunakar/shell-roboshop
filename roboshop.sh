36f9003283aa575312557aef76b0018e3ee66cc4
unknown <karunakark1729@gmail.com>
2025-05-27 15:21:41 +0530

    Udate r53 record updated sg-id, access-key

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="karna.fun"
 
 for instance in ${INSTANCES[@]}
 # for instance in $@; do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0738b5ce8c030b4a6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0738b5ce8c030b4a6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
@@ -18,21 +18,21 @@ for instance in ${INSTANCES[@]}
     fi
     echo "$instance IP address: $IP"
 
-    # aws route53 change-resource-record-sets \
-    # --hosted-zone-id $ZONE_ID \
-    # --change-batch '
-    # {
-    #     "Comment": "Creating or Updating a record set for cognito endpoint"
-    #     ,"Changes": [{
-    #     "Action"              : "UPSERT"
-    #     ,"ResourceRecordSet"  : {
-    #         "Name"              : "'$RECORD_NAME'"
-    #         ,"Type"             : "A"
-    #         ,"TTL"              : 1
-    #         ,"ResourceRecords"  : [{
-    #             "Value"         : "'$IP'"
-    #         }]
-    #     }
-    #     }]
-    # }'
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
 done
